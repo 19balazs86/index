8949b01890eed0ff6d55194b53624963a61578b2
Balázs Keresztúri <19balazs86@gmail.com>
2019-02-02 11:25:50 +0000

    Add clone-repositories.bat.

---
+++ other/clone-repositories.bat
@@ -0,0 +1,16 @@
+git clone https://github.com/19balazs86/index.git
+git clone https://github.com/19balazs86/PlayingWithAggregateException.git
+git clone https://github.com/19balazs86/PlayingWithDistributedLock.git
+git clone https://github.com/19balazs86/PlayingWithGenericHost.git
+git clone https://github.com/19balazs86/PlayingWithHealthChecks.git
+git clone https://github.com/19balazs86/PlayingWithHttpClientFactory.git
+git clone https://github.com/19balazs86/Playing-with-JWT-Authentication.git
+git clone https://github.com/19balazs86/PlayingWithMediatR.git
+git clone https://github.com/19balazs86/PlayingWithMongoDB.git
+git clone https://github.com/19balazs86/PlayingWithObserver.git
+git clone https://github.com/19balazs86/PlayingWithOcelot.git
+git clone https://github.com/19balazs86/PlayingWithRefit.git
+git clone https://github.com/19balazs86/Playing-with-Serilog.git
+git clone https://github.com/19balazs86/PlayingWithSwagger.git
+git clone https://github.com/19balazs86/PlayingWithTestHost.git
+pause
