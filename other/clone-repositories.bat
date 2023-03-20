24968402f5b5e9311b1d44f298068c71c88d951c
Balazs Kereszturi <19balazs86@gmail.com>
2023-03-20 10:03:33 +0100

    Clone-repo: Add AzureStaticWebAppBlazorPolling

--- other/clone-repositories.bat
+++ other/clone-repositories.bat
@@ -3,6 +3,7 @@ git clone https://github.com/19balazs86/AzureContainerApp.git
 git clone https://github.com/19balazs86/AzureDevOps.git
 git clone https://github.com/19balazs86/AzureFunctions.git
 git clone https://github.com/19balazs86/AzureServiceBus.git
+git clone https://github.com/19balazs86/AzureStaticWebAppBlazorPolling
 git clone https://github.com/19balazs86/index.git
 git clone https://github.com/19balazs86/PlayingWith_gRPC.git
 git clone https://github.com/19balazs86/PlayingWithAggregateException.git
